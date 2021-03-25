eaa8da0b60fb37eaadefe97c45d717fc2b06f08c
Ksam <kevsam42@gmail.com>
2021-03-25 14:19:49 +0100

    fix: syntax of swap funciton

--- srcs/basic_sort.c
+++ srcs/basic_sort.c
@@ -3,26 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   basic_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tglory <tglory@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: ksam <ksam@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 13:08:51 by tglory            #+#    #+#             */
-/*   Updated: 2021/03/25 13:38:59 by tglory           ###   ########lyon.fr   */
+/*   Updated: 2021/03/25 14:18:32 by ksam             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	ft_swap(int **list)
+void	ft_swap(int *list, size_t length)
 {
-	size_t length;
-	int	*swap;
+	int	swap;
 	int	index;
-	
-	length = sizeof(list) / sizeof(int);
+
 	if (length < 2)
 		return;
 	index = length - 1;
-	*swap = *list[index];
-	*list[index] = *list[index - 1];
-	*list[index - 1] = *swap;
+	swap = list[index];
+	list[index] = list[index - 1];
+	list[index - 1] = swap;
 }
--- srcs/main.c
+++ srcs/main.c
@@ -3,23 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tglory <tglory@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: ksam <ksam@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 12:58:02 by tglory            #+#    #+#             */
-/*   Updated: 2021/03/25 13:39:55 by tglory           ###   ########lyon.fr   */
+/*   Updated: 2021/03/25 14:15:06 by ksam             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	print_int_array(int **list)
+void	print_int_array(int *list, size_t length)
 {
-	size_t	length;
+	int		index = 0;
 
-	length = sizeof(list) / sizeof(int);
-	printf("list (%zu elements) : ", *length);
-	while (length > 0)
-		printf("%d ", *list[length--]);
+	printf("list (%zu elements) : ", length);
+	while (length > index)
+		printf("%d ", list[index++]);
 	printf("\n");
 }
 
@@ -27,9 +26,9 @@ int	main(void)
 {
 	int	a[] = {9, 5, 4, 3, 2};
 	int	b[] = { };
-
-	print_int_array(a);
-	ft_swap(a);
-	print_int_array(a);
+	size_t length = sizeof(a) / sizeof(int);
+	print_int_array(a, length);
+	ft_swap(a, length);
+	print_int_array(a, length);
 	return (0);
 }

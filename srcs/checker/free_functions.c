9c5216ed1bf91283fd9df919bbfc6091acc3c561
tglory <tglory@student.42Lyon.fr>
2021-05-25 12:29:30 +0200

    change headers

--- srcs/checker/free_functions.c
+++ srcs/checker/free_functions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_functions.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksam <ksam@student.42lyon.fr>              +#+  +:+       +#+        */
+/*   By: tglory <tglory@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/13 16:02:41 by ksam              #+#    #+#             */
-/*   Updated: 2021/05/24 17:20:06 by ksam             ###   ########lyon.fr   */
+/*   Created: 2021/05/13 16:02:41 by tglory            #+#    #+#             */
+/*   Updated: 2021/05/25 12:28:14 by tglory           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	free_all(int *number_to_order, t_stack_master *stack_master)
 		free(stack_master);
 }
 
-void	free_manager(t_master *manager, char ** instructions)
+void	free_manager(t_master *manager, char **instructions)
 {
 	while (manager->count)
 		data_eraser(manager, manager->first, 1);
